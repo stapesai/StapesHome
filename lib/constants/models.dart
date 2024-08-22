5432975d7cf9b0a7d112c322e178dda822aa58b4
Devasheesh Mishra <devasheeshmishra4@gmail.com>
2024-08-22 10:38:16 +0000

    refactor: Update models for Floor, Room, and Device

--- lib/constants/models.dart
+++ lib/constants/models.dart
@@ -1,33 +1,36 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
 class Floor {
   final String id;
-  final String name;
+  final int level;
+  final String alias;
 
-  Floor({required this.id, required this.name});
+  Floor({required this.id, required this.level, required this.alias});
 }
 
 class Room {
   final String id;
+  final String floorId;
   final String name;
+  final String type;
 
-  Room({required this.id, required this.name});
+  Room({required this.id, required this.floorId, required this.name, required this.type});
 }
 
-class Device {
-  final String id;
-  final String name;
-  final IconData icon;
-  final bool isActive;
-  final bool hasSlider;
-  final double sliderValue;
+// class Device {
+//   final String id;
+//   final String name;
+//   final IconData icon;
+//   final bool isActive;
+//   final bool hasSlider;
+//   final double sliderValue;
 
-  Device({
-    required this.id,
-    required this.name,
-    required this.icon,
-    this.isActive = false,
-    this.hasSlider = false,
-    this.sliderValue = 0.0,
-  });
-}
+//   Device({
+//     required this.id,
+//     required this.name,
+//     required this.icon,
+//     this.isActive = false,
+//     this.hasSlider = false,
+//     this.sliderValue = 0.0,
+//   });
+// }
