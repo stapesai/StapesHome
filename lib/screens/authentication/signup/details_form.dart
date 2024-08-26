aa8a104286293dd7ee42cd84235857aa148f5f01
Devasheesh Mishra <devasheeshmishra4@gmail.com>
2024-08-26 16:59:00 +0000

    refactor: Update email input UI and functionality in signup page

---
+++ lib/screens/authentication/signup/details_form.dart
@@ -0,0 +1,213 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+import 'package:jarvis/utils/hive.dart';
+import 'package:jarvis/utils/sessions_model.dart';
+import 'package:jarvis/widgets/button.dart'; // Import the CustomButton widget
+import 'package:jarvis/constants/colors.dart';
+import 'package:jarvis/widgets/input_fields.dart';
+import 'package:jarvis/constants/api_routes.dart';
+import 'package:jarvis/screens/routes/main.dart';
+
+class SignupForm extends StatefulWidget {
+  final String passWord;
+  final String email;
+  final TextEditingController first_name = TextEditingController();
+  final TextEditingController last_name = TextEditingController();
+  final TextEditingController dob = TextEditingController();
+  final TextEditingController gender = TextEditingController();
+  final String transaction_id;
+  SignupForm({
+    super.key,
+    required this.passWord,
+    required this.transaction_id,
+    required this.email,
+  });
+
+  @override
+  createState() => _SignupFormState();
+}
+
+class _SignupFormState extends State<SignupForm> {
+  bool _isLoading = false;
+  final HiveService hiveService = HiveService();
+
+  Future<void> handleConfirmSignup(
+      BuildContext context, String email, String first_name, String last_name, String dob, String gender) async {
+    setState(() {
+      _isLoading = true;
+    });
+    var check_email_response = await http.post(
+      AuthRoutes.checkEmail(email),
+      headers: {'accept': 'application/json'},
+    );
+    var check_email_responseBody = json.decode(check_email_response.body);
+
+    if (check_email_response.statusCode == 200) {
+      var response = await http.post(
+        AuthRoutes.completeSignup,
+        headers: {'Content-Type': 'application/json', 'accept': 'application/json'},
+        body: jsonEncode({
+          "user": {
+            "email": email,
+            "first_name": first_name,
+            "last_name": last_name,
+            "dob": dob,
+            "gender": gender,
+          },
+          "password": widget.passWord,
+          "transaction_id": widget.transaction_id,
+        }),
+      );
+      var responseBody = json.decode(response.body);
+
+      if (response.statusCode == 200) {
+        var sessionData = SessionsModel(
+          sessionId: responseBody['session']['session_id'],
+          userId: responseBody['session']['user_id'],
+          createdAt: DateTime.parse(responseBody['session']['created_at']),
+          lastActiveAt: DateTime.parse(responseBody['session']['last_active_at']),
+        );
+
+        await hiveService.addBoxes([sessionData], "SessionBox");
+        if (context.mounted) {
+          Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                builder: (context) => MainScreen(),
+              ));
+        }
+      } else {
+        if (context.mounted) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text('Error : ${response.statusCode} - ${responseBody["detail"]} '),
+            ),
+          );
+        }
+      }
+    } else {
+      if (context.mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Error : ${check_email_response.statusCode} - ${check_email_responseBody["detail"]} '),
+          ),
+        );
+      }
+    }
+
+    setState(() {
+      _isLoading = false; // Stop loading indicator
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+    final keyboardHeight = MediaQuery.of(context).viewInsets.bottom;
+
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Container(
+        clipBehavior: Clip.antiAlias,
+        decoration: ShapeDecoration(
+          gradient: AppColor.backgroundColorgradient,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30),
+          ),
+        ),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          resizeToAvoidBottomInset: false,
+          body: _isLoading
+              ? const Center(
+                  child: CircularProgressIndicator(
+                    color: AppColor.whiteColor,
+                  ),
+                )
+              : SafeArea(
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: screenSize.width * 0.05),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(height: screenSize.height * 0.08),
+                        SizedBox(
+                          width: double.infinity,
+                          child: Text(
+                            'Sign Up',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: screenSize.width * 0.1,
+                              fontFamily: 'Ubuntu',
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: screenSize.height * 0.02),
+                        SizedBox(
+                          width: screenSize.width * 0.9,
+                          child: Text(
+                            'Please enter your personal details.',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: screenSize.width * 0.04,
+                              fontFamily: 'Ubuntu',
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: screenSize.height * 0.04),
+                        NTextField(
+                          hintText: 'First Name',
+                          controller: widget.first_name,
+                        ),
+                        SizedBox(height: screenSize.height * 0.02),
+                        NTextField(
+                          hintText: 'Last Name',
+                          controller: widget.last_name,
+                        ),
+                        SizedBox(height: screenSize.height * 0.02),
+                        NTextField(
+                          hintText: 'Date of Birth',
+                          controller: widget.dob,
+                        ),
+                        SizedBox(height: screenSize.height * 0.02),
+                        NTextField(
+                          hintText: 'Gender',
+                          controller: widget.gender,
+                        ),
+                        const Spacer(),
+                        AnimatedContainer(
+                          duration: const Duration(milliseconds: 300),
+                          curve: Curves.easeOut,
+                          margin: EdgeInsets.only(
+                            bottom: keyboardHeight > 0
+                                ? keyboardHeight + screenSize.height * 0.02
+                                : screenSize.height * 0.1,
+                          ),
+                          child: Center(
+                            child: CustomButton(
+                              text: "Done",
+                              onPressed: () {
+                                handleConfirmSignup(
+                                  context,
+                                  widget.email,
+                                  widget.first_name.text,
+                                  widget.last_name.text,
+                                  widget.dob.text,
+                                  widget.gender.text,
+                                );
+                              },
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/authentication/signup/email_input.dart
@@ -0,0 +1,210 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+import 'package:jarvis/constants/api_routes.dart';
+import 'package:jarvis/widgets/button.dart';
+import 'package:jarvis/constants/colors.dart';
+import 'package:jarvis/widgets/input_fields.dart';
+import 'package:jarvis/screens/authentication/password.dart';
+import 'package:jarvis/screens/authentication/otp_verify.dart';
+import 'package:jarvis/screens/authentication/signup/details_form.dart';
+
+class EmailSignUp extends StatefulWidget {
+  const EmailSignUp({super.key});
+
+  @override
+  createState() => _EmailSignUpState();
+}
+
+class _EmailSignUpState extends State<EmailSignUp> {
+  final TextEditingController emailController = TextEditingController();
+  bool _isLoading = false;
+  Future<void> handlerequestSignup(BuildContext context, String email) async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    var check_email_response = await http.post(
+      AuthRoutes.checkEmail(email),
+      headers: {'accept': 'application/json'},
+    );
+    var check_email_responseBody = json.decode(check_email_response.body);
+
+    if (check_email_response.statusCode == 200) {
+      var response = await http.post(
+        AuthRoutes.requestSignup,
+        headers: {'Content-Type': 'application/json', 'accept': 'application/json'},
+        body: jsonEncode({
+          'email': email,
+        }),
+      );
+      var responseBody = json.decode(response.body);
+
+      if (response.statusCode == 200) {
+        String transactionId = responseBody['transaction_id'];
+        print('test trans_id' + transactionId);
+        if (context.mounted) {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+              builder: (context) => OtpVerificationScreen(
+                transactionId: transactionId,
+                time: DateTime.parse(responseBody["otp_expires_at"]),
+                onSuccess: () async {
+                  if (context.mounted) {
+                    print('inside context' + transactionId);
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => PasswordScreen(
+                          title: 'Create Password',
+                          subtitle: 'Lets create a password to secure your account.',
+                          nextScreen: SignupForm(
+                            passWord: '',
+                            transaction_id: '',
+                            email: '',
+                          ),
+                          email: email,
+                          transaction_id: transactionId,
+                        ),
+                      ),
+                    );
+                  } else {
+                    print('Error :  ${responseBody} ');
+                    if (context.mounted) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text('Error : ${response.statusCode} - ${responseBody["detail"]} '),
+                        ),
+                      );
+                    }
+                  }
+                },
+                onError: () {
+                  print('Error :  ${responseBody["detail"]} ');
+                  if (context.mounted) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text('Error : ${response.statusCode} - ${responseBody["detail"]} '),
+                      ),
+                    );
+                  }
+                },
+              ),
+            ),
+          );
+        }
+      } else {
+        if (context.mounted) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text('Error : ${response.statusCode} - ${responseBody["detail"]} '),
+            ),
+          );
+        }
+      }
+    } else {
+      if (context.mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Error : ${check_email_response.statusCode} - ${check_email_responseBody["detail"]} '),
+          ),
+        );
+      }
+    }
+
+    setState(() {
+      _isLoading = false; // Stop loading indicator
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+    final keyboardHeight = MediaQuery.of(context).viewInsets.bottom;
+
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Container(
+        clipBehavior: Clip.antiAlias,
+        decoration: ShapeDecoration(
+          gradient: AppColor.backgroundColorgradient,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30),
+          ),
+        ),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          resizeToAvoidBottomInset: false,
+          body: _isLoading
+              ? const Center(
+                  child: CircularProgressIndicator(
+                    color: AppColor.whiteColor,
+                  ),
+                )
+              : SafeArea(
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: screenSize.width * 0.05),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(height: screenSize.height * 0.08),
+                        SizedBox(
+                          width: double.infinity,
+                          child: Text(
+                            'Sign Up',
+                            style: TextStyle(
+                              color: AppColor.whiteColor,
+                              // fontSize: screenSize.width * 0.1,
+                              fontSize: 44,
+                              fontFamily: 'Ubuntu',
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: screenSize.height * 0.02),
+                        SizedBox(
+                          child: Text(
+                            'Enter your email to receive verification code.',
+                            style: TextStyle(
+                              color: AppColor.whiteColor,
+                              // fontSize: screenSize.width * 0.04,
+                              fontSize: 20,
+                              fontFamily: 'Ubuntu',
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: screenSize.height * 0.04),
+                        SizedBox(
+                          child: NTextField(
+                            hintText: 'Enter your email',
+                            controller: emailController,
+                            icon: Icons.email_rounded,
+                          ),
+                        ),
+                        const Spacer(),
+                        AnimatedContainer(
+                          duration: const Duration(milliseconds: 300),
+                          curve: Curves.easeOut,
+                          margin: EdgeInsets.only(
+                            bottom: keyboardHeight > 0
+                                ? keyboardHeight + screenSize.height * 0.02
+                                : screenSize.height * 0.1,
+                          ),
+                          child: Center(
+                            child: CustomButton(
+                              text: "Send Code",
+                              onPressed: () => handlerequestSignup(context, emailController.text),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+        ),
+      ),
+    );
+  }
+}
