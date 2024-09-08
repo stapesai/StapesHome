10e0dd3c18efe21cd818dd8c8103317cd4f29fa6
Gauransh Sharma <gs7148@srmist.edu.in>
2024-09-08 23:13:01 +0530

    Refactor colors.dart file and remove unused color constants

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -8,13 +8,14 @@ class AppColor {
   static const Color backgroundColorDark = Color(0xFF161622);
   static const instructionPanelColor = Color(0xFF181819);
   static const Color primaryColor = Color(0xFFFF9F1C);
-  static const Color primaryColorLight = Color(0XFF898989);
-  static const Color secondaryColor = Color(0xFF141414);
-  static const Color containerColor = Color(0xFF28282F);
+  static const Color errorColor = Colors.red;
+  // static const Color primaryColorLight = Color(0XFF898989);
+  // static const Color secondaryColor = Color(0xFF141414);
+  // static const Color containerColor = Color(0xFF28282F);
   static const Color iconBarColor = Color.fromARGB(152, 255, 160, 28);
   static const backgroundColorgradient = LinearGradient(
       begin: Alignment(0.00, -1.00), end: Alignment(0, 1), colors: [Color(0xFF353841), Color(0xFF141414)]);
-  static const Color textFieldbgColor = Color(0xFF28282F);
+  // static const Color textFieldbgColor = Color(0xFF28282F);
   // static const Color themecolor = Color(0XFFC2F656);
   static const Color grayColor = Color(0xFF898989);
 
