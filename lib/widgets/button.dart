6a5a1c372f8787c61165b4418ab6fd9f34b2816f
Devasheesh Mishra <devasheeshmishra4@gmail.com>
2024-11-11 13:14:49 +0000

    added loading parm in custom button

--- lib/widgets/button.dart
+++ lib/widgets/button.dart
@@ -4,12 +4,14 @@ import 'package:stapes_home/core/theme/app_colors.dart';
 class CustomButton extends StatelessWidget {
   final String text;
   final VoidCallback onPressed;
+  final bool isLoading;
   final Color backgroundColor = AppColor.iconBarColor;
 
   const CustomButton({
     super.key,
     required this.text,
     required this.onPressed,
+    this.isLoading = false,
   });
 
   @override
@@ -17,23 +19,28 @@ class CustomButton extends StatelessWidget {
     return SizedBox(
       height: 54, // Fixed height
       width: double.infinity, // Takes maximum available width
-      child: ElevatedButton.icon(
-        onPressed: onPressed,
-        label: Text(
-          text,
-          style: const TextStyle(
-            fontSize: 18.0,
-            fontWeight: FontWeight.w400,
-            color: AppColor.whiteColor,
-          ),
-        ),
+      child: ElevatedButton(
+        // if isLoading is true, onPressed will be null
+        onPressed: isLoading ? null : onPressed,
         style: ElevatedButton.styleFrom(
           backgroundColor: backgroundColor,
-          // padding: const EdgeInsets.symmetric(horizontal: 40.0, vertical: 15.0),
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(30.0),
           ),
         ),
+        child: isLoading
+            ? const CircularProgressIndicator(
+                strokeWidth: 2.0,
+                valueColor: AlwaysStoppedAnimation<Color>(AppColor.whiteColor),
+              )
+            : Text(
+                text,
+                style: const TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.w400,
+                  color: AppColor.whiteColor,
+                ),
+              ),
       ),
     );
   }
