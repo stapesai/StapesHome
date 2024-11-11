06fc1552c02aff02729a586803f4eb30df5e86ad
Devasheesh Mishra <devasheeshmishra4@gmail.com>
2024-11-11 13:13:05 +0000

    Add ButtonCubit and refactor button state management; remove unused ButtonStateCubit

--- lib/core/common/bloc/button/button_state.dart
+++ lib/core/common/bloc/button/button_state.dart
@@ -1,15 +1,15 @@
 // Path: lib/core/common/bloc/button/button_state.dart
 // Description: This file contains the state for the button bloc.
 
-abstract class ButtonState {}
+// abstract class ButtonState {}
 
-class ButtonInitialState extends ButtonState {}
+// class ButtonInitialState extends ButtonState {}
 
-class ButtonLoadingState extends ButtonState {}
+// class ButtonLoadingState extends ButtonState {}
 
-class ButtonSuccessState extends ButtonState {}
+// class ButtonSuccessState extends ButtonState {}
 
-class ButtonFailureState extends ButtonState {
-  final String errorMessage;
-  ButtonFailureState({required this.errorMessage});
-}
+// class ButtonFailureState extends ButtonState {
+//   final String errorMessage;
+//   ButtonFailureState({required this.errorMessage});
+// }
