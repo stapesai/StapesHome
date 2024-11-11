88f07c64cc18a30605ea97da6bb0678eb2ff1fe1
Devasheesh Mishra <devasheeshmishra4@gmail.com>
2024-11-11 13:47:12 +0000

    created login cubit and state

---
+++ lib/presentation/auth/bloc/login_state.dart
@@ -0,0 +1,46 @@
+// Path: lib/presentation/auth/bloc/login_state.dart
+// Description: This file contains the state for the login bloc.
+
+import 'package:equatable/equatable.dart';
+
+abstract class LoginState extends Equatable {
+  const LoginState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class LoginInitial extends LoginState {}
+
+class LoginLoading extends LoginState {}
+
+class LoginOtpRequired extends LoginState {
+  final String transactionId;
+  final DateTime expiryTime;
+
+  const LoginOtpRequired({
+    required this.transactionId,
+    required this.expiryTime,
+  });
+
+  @override
+  List<Object> get props => [transactionId, expiryTime];
+}
+
+class LoginError extends LoginState {
+  final String message;
+
+  const LoginError(this.message);
+
+  @override
+  List<Object> get props => [message];
+}
+
+class LoginSuccess extends LoginState {
+  final String message;
+
+  const LoginSuccess(this.message);
+
+  @override
+  List<Object> get props => [message];
+}
