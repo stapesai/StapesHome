e7a2da49ade7609d340c487256028b9e4d263c32
Devasheesh Mishra <devasheeshmishra4@gmail.com>
2024-11-15 22:40:15 +0000

    failed attempts to fix scroll physics to make scrolling snappy.

--- lib/features/navigation/presentation/pages/navigation_screen.dart
+++ lib/features/navigation/presentation/pages/navigation_screen.dart
@@ -23,6 +23,8 @@ class _NavigationScreenState extends State<NavigationScreen> {
   late final PageController _pageController;
   late final NavigationBloc _navigationBloc;
   bool _isHandlingTap = false;
+  double _dragStart = 0.0;
+  double _dragOffset = 0.0;
 
   @override
   void initState() {
@@ -40,6 +42,12 @@ class _NavigationScreenState extends State<NavigationScreen> {
 
   @override
   Widget build(BuildContext context) {
+    // Get screen width
+    final double screenWidth = MediaQuery.of(context).size.width;
+
+    // TODO: i dont understand why we can't use BlocProvider directly here.
+    // BlocProvider should not rerender as the Scaffold is its child.
+    // So, on any event maximum Scaffold will rerender.
     return BlocProvider.value(
       value: _navigationBloc,
       // return BlocProvider(
@@ -68,9 +76,50 @@ class _NavigationScreenState extends State<NavigationScreen> {
         },
         child: Scaffold(
           backgroundColor: Colors.transparent,
+          //   body: GestureDetector(
+          //     onHorizontalDragStart: (details) {
+          //       _dragStart = details.globalPosition.dx;
+          //       _dragOffset = _pageController.offset;
+          //     },
+          //     onHorizontalDragUpdate: (details) {
+          //       final currentDrag = details.globalPosition.dx;
+          //       final dragDifference = currentDrag - _dragStart;
+
+          //       // Calculate new position while dragging
+          //       final newOffset = _dragOffset - dragDifference;
+          //       final maxOffset = screenWidth * (NavigationTab.values.length - 1);
+
+          //       // Clamp the offset to prevent overscrolling
+          //       final clampedOffset = newOffset.clamp(0.0, maxOffset);
+
+          //       // Update page position in real-time
+          //       _pageController.jumpTo(clampedOffset);
+          //     },
+          //     onHorizontalDragEnd: (details) {
+          //       final dragEnd = details.primaryVelocity ?? 0;
+          //       final dragDistance = details.globalPosition.dx - _dragStart;
+          //       final currentPage = (_pageController.offset / screenWidth).round();
+          //       int targetPage = currentPage;
+
+          //       if (dragDistance.abs() > screenWidth / 2 || dragEnd.abs() > 800) {
+          //         if (dragDistance < 0 && currentPage < NavigationTab.values.length - 1) {
+          //           targetPage = currentPage + 1;
+          //         } else if (dragDistance > 0 && currentPage > 0) {
+          //           targetPage = currentPage - 1;
+          //         }
+          //       }
+
+          //       final selectedItem = NavigationTab.values[targetPage];
+          //       _navigationBloc.add(NavigationPageSwiped(selectedItem));
+          //     },
+          // child: PageView(
           body: PageView(
             controller: _pageController,
             // physics: const BouncingScrollPhysics(),
+            // physics: const ClampingScrollPhysics(),
+            // This will disable the swipe gesture of the PageView.
+            // physics: const NeverScrollableScrollPhysics(),
+
             onPageChanged: (index) {
               // Update navigation state when page is swiped
               if (!_isHandlingTap) {
@@ -91,6 +140,7 @@ class _NavigationScreenState extends State<NavigationScreen> {
               DevTestPage(text: 'Settings Page'),
             ],
           ),
+          // ),
           bottomNavigationBar: const CustomNavigationBar(),
         ),
       ),
