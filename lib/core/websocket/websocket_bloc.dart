2d962c7fe995315839702538300bdfed78f35c42
Devasheesh Mishra <devasheeshmishra4@gmail.com>
2024-11-17 14:26:04 +0000

    .

--- lib/core/websocket/websocket_bloc.dart
+++ lib/core/websocket/websocket_bloc.dart
@@ -9,6 +9,8 @@ import 'websocket_state.dart';
 class WebsocketBloc extends Bloc<WebsocketEvent, WebsocketState> {
   // FIXME: We have to do this as there was an issue in the app that only the home page was being loaded by default,
   // so only that page is receiving web socket events by default, until user opens the other pages, they don't receive the events.
+  // For now, we can solve this issue by keeping the history of the messages and emitting them to all the pages.
+  // But, this is not a good solution. We have to find a better solution for this.
   final List<WebsocketIncommingMessage> messagesHistory = [];
   final WebsocketService _webSocketService;
   StreamSubscription<dynamic>? _messageSubscription;
